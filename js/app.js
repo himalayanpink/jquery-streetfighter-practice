4d0eb9828f9219542b861a46c310318f245c8609
himalayanpink <cac.calman@gmail.com>
2015-09-29 22:25:23 -0600

    fixed some concurrent issues

--- js/app.js
+++ js/app.js
@@ -1,51 +1,63 @@
 $(document).ready(function() {
+	
+	var isCool = false;
 
     $(".ryu-box")
    		.mouseenter(function() {
+        	if (isCool){
+        		return;
+        	}
         	$(".standing").hide();
    			$(".ready").show();
     	})
     	.mouseleave(function() {
-  			$(".standing").show();
-   			$(".ready").hide();
+    		if (!isCool){
+  				$(".standing").show();
+   				$(".ready").hide();
+   			}
     	})
     	.mousedown(function() {
-    		$(".throwing").show();
-    		$(".ready").hide();
-    		$(".hadouken")
-    			.finish()
-    			.show()
-    			.animate(
-    			{ "left": "2000px" }, "slow", function() {
-    				$(this).hide();
-    				$(this).css("left", "500px");
-    			});
-		  	$("#hadouken-sound").load().play();
-
+    		if (!isCool){
+    			$(".throwing").show();
+    			$(".ready").hide();
+    			$(".hadouken")
+    				.finish()
+    				.show()
+    				.animate(
+    					{ "left": "2000px" }, "slow", function() {
+    						$(this).hide();
+							$(this).css("left", "500px");
+    				});
+		  		$("#hadouken-sound").load().play();
+		  	}
     	})
     	.mouseup(function() {
-    		$(".throwing").hide();
-    		$(".ready").show();
+    		if (!isCool){
+    			$(".throwing").hide();
+    			$(".ready").show();
+    		}
     	})    
     ;
 
 	$(this)
 		.keydown(function(e) {
     		if (e.which == 88) {
+    			isCool = true;
     			$(".cool").show();
    				$(".standing").hide();
    				$(".throwing").hide();
     			$(".ready").hide();
-    			$(".hadouken").hide()
+    			$(".hadouken").hide();
 			}
 		})
 		.keyup(function(e) {
     		if (e.which == 88) {
+    			isCool = false;
     			$(".cool").hide();
    				$(".standing").show();
    				$(".throwing").hide();
     			$(".ready").hide();
-    			$(".hadouken").hide()
+    			$(".hadouken").hide();
 			}
 		})
 	;
@@ -54,8 +66,8 @@ $(document).ready(function() {
 
 //function playHadouken() {
 //	$("#hadouken-sound")[0].volume = 0.5;
-//	$("#hadouken-sound")[0].load ();
-//	$("#hadouken-sound")[0].play ();
+//	$("#hadouken-sound")[0].load();
+//	$("#hadouken-sound")[0].play();
 //}
 
 
